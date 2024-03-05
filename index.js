0bb83efdb519deec03f7e7bbc8c704dd342dd738
Mel Müller <126467515+melmuller@users.noreply.github.com>
2024-03-05 11:24:54 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//comando para exibir mensagem
+
+console.log("O INTER VAI SAIR CAMPEÃO E O GRÊMIO NÃO")
+
+//exercicio de calculos
+
+const num1 = 1909;
+const num2 = 654;
+
+//soma 
+let soma = num1 + num2;
+
+console.log("O valor da soma é " + soma + " Parabéns");
