7c97ebe61ebca52699571d60077655bcfc6f158b
Mel Müller <126467515+melmuller@users.noreply.github.com>
2024-03-08 11:23:38 -0300

    Add files via upload

---
+++ voto.js
@@ -0,0 +1,12 @@
+const idade = 16;
+if (idade < 16) {
+    console.log("Não possui idade para votar");
+}
+
+else if (idade => 16 && idade < 18) {
+    console.log("Voto facultativo");
+}
+
+else if(idade <=18) {
+    console.log("Voto obeigatório");
+}
